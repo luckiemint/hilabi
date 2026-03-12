71496052d7f0437aded23d0e4f5189c0114e6918
luckiemint <luckiemint@gmail.com>
2026-03-12 21:50:40 +0530

    push ui style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {  useEffect, useRef } from "react";
 import LandingLayout from "../../components/LandingLayout/LandingLayout";
 import {
   QrCode,
